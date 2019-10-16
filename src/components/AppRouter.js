47db6d33e5962f7647d039fb5a40ad932ec5175e
Сотников Игорь Юрьевич <mxtfonlife@mail.ru>
2019-10-16 16:15:43 +0700

    Make small changes

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,7 +2,7 @@ import React, { lazy, useMemo, Suspense } from 'react'
 
 export default function AppRouter({ appName, match }) {    
 
-    const App = useMemo(() => lazy(() => import(`/public/apps/${appName}/index.js`)), [appName])
+    const App = useMemo(() => lazy(() => System.import(`/public/apps/${appName}/index.js`)), [appName])
 
     return (
         <Suspense fallback={'Загрузка...'}>
