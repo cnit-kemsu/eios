2c4379c8325eabea80b31bc8793ca270f2107066
Сотников Игорь Юрьевич <mxtfonlife@mail.ru>
2019-10-29 15:48:58 +0700

    Replace route path with regex

--- src/Main.js
+++ src/Main.js
@@ -6,13 +6,13 @@ import AppRouter from './core/AppRouter'
 
 const routes = {
     '/': '/home',
-    '/:appName/*': ({ appName }, match) => <AppRouter appName={appName} match={match} />    
+    '@/([a-zA-Z0-9_-]+)((/([a-zA-Z0-9_-]+))*)': ({ 0: appName }, match) => <AppRouter appName={appName} match={match} />
 }
 
 export default function Main() {
 
     const route = useRoutes(routes)
-    
+
     return (
         <ErrorBoundary>
             {route}
