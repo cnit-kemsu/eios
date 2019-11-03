b945eaca32f4ef19c741d8b8944c6f850b49368e
Сотников Игорь Юрьевич <mxtfonlife@mail.ru>
2019-11-04 00:24:24 +0700

    Replace keyframes with transition

--- src/share/layout/AppContent/style.js
+++ src/share/layout/AppContent/style.js
@@ -1,40 +1,17 @@
-import { css, keyframes } from '@emotion/core'
+import { css } from '@emotion/core'
 
 import { offset, transitionDuration } from '../constants'
 
 export const rootCss = css`
     flex: 1 1 auto;
     margin: ${offset}; 
-`
-
-const fadeInKf = keyframes`
-    from {
-        opacity: 0;
-    }
-    to {
-        opacity: 1;
-    }
-`
-
-const fadeOutKf = keyframes`
-    from {
-        opacity: 1;
-    }
-    to {
-        opacity: 0;
-    }
+    transition: opacity ${transitionDuration};
 `
 
 export const fadeInCss = css`
-    animation-name: ${fadeInKf};
-    animation-duration: ${transitionDuration};
-    animation-fill-mode: forwards;
-    animation-timing-function: ease-in-out;
+    opacity: 1;
 `
 
 export const fadeOutCss = css`
-    animation-name: ${fadeOutKf};
-    animation-duration: ${transitionDuration};
-    animation-fill-mode: forwards;
-    animation-timing-function: ease-in-out;
+    opacity: 0;
 `
