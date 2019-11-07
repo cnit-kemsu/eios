cd78c635875ddbdb30b65774119bcd76777d2d34
Сотников Игорь Юрьевич <mxtfonlife@mail.ru>
2019-11-07 09:51:01 +0700

    modified:   src/share/utils/fetchApi.js

--- src/share/utils/fetchApi.js
+++ src/share/utils/fetchApi.js
@@ -2,12 +2,16 @@ const apiUriPrefix = 'https://api-next.kemsu.ru/api'
 const apiDevUriPrefix = 'https://api-dev.kemsu.ru/api'
 
 
+function fetchApiGeneral(url, options = {}) {   
+    return fetch(url, options)
+}
+
 export function fetchApi(uri, options) {
-    return fetch(uri[0] === '/' ? apiUriPrefix + uri : apiUriPrefix + '/' + uri, options)
+    return fetchApiGeneral(uri[0] === '/' ? apiUriPrefix + uri : apiUriPrefix + '/' + uri, options);
 }
 
 export function fetchDevApi(uri, options) {
-    return fetch(uri[0] === '/' ? apiDevUriPrefix + uri : apiDevUriPrefix + '/' + uri, options)
+    return fetchApiGeneral(uri[0] === '/' ? apiDevUriPrefix + uri : apiDevUriPrefix + '/' + uri, options)
 }
 
 
