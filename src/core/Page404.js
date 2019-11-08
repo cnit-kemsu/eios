833e61835bcc97ff083dfab977688a1f286ddb73
Сотников Игорь Юрьевич <mxtfonlife@mail.ru>
2019-11-08 10:42:22 +0700

    Update Page404

--- src/core/Page404.js
+++ src/core/Page404.js
@@ -3,6 +3,6 @@ import React from 'react'
 export default function Page404() {
 
     return (
-        <div>Ошибка 404: Страница не найдена!</div>
+        <div style={{ color: 'white', background: 'red' }}>Ошибка 404: Страница не найдена!</div>
     )
 }
