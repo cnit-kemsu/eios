b77e14800376dd4c09de5d13e1bc3c1a27bbc960
Сотников Игорь Юрьевич <mxtfonlife@mail.ru>
2019-11-12 16:53:39 +0700

    Update dekanat app

--- src/apps/dekanat/index.js
+++ src/apps/dekanat/index.js
@@ -1,11 +1,12 @@
 import * as dekanatPageModule from './DekanatPage'
+import { makeAppPageGenerator } from 'share/utils'
 
-
-export async function getAppPage() {
-    if (location.pathname === '/dekanat') return dekanatPageModule
-    return await import(                          
-        `./${location.pathname.split('/').slice(2).join('/')}/index.js`
-    )
-}
+export const getAppPage = makeAppPageGenerator('dekanat', dekanatPageModule, {    
+    footerContactInfo: {
+        phone: '(384-2) 58-32-89, 58-33-41, 58-44-03',
+        localPhone: '4-62, 3-46, 4-65',
+        email: 'ocpo@kemsu.ru'
+    }
+})
 
 export { default as Layout } from '../../share/layout/Layout'
