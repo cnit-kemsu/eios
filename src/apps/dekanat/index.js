c8da7c0077ea0cfbc210655f3282df00e97bdef9
Сотников Игорь Юрьевич <mxtfonlife@mail.ru>
2019-11-13 16:06:06 +0700

    Update apps

--- src/apps/dekanat/index.js
+++ src/apps/dekanat/index.js
@@ -1,12 +1,24 @@
+import React from 'react'
+
 import * as dekanatPageModule from './DekanatPage'
 import { makeAppPageGenerator } from 'share/utils'
 
-export const getAppPage = makeAppPageGenerator('dekanat', dekanatPageModule, {    
-    footerContactInfo: {
-        phone: '(384-2) 58-32-89, 58-33-41, 58-44-03',
-        localPhone: '4-62, 3-46, 4-65',
-        email: 'ocpo@kemsu.ru'
+export const getAppPage = makeAppPageGenerator('dekanat', dekanatPageModule, {
+    layoutProps: {
+        footerContactInfo: {
+            phone: '(384-2) 58-32-89, 58-33-41, 58-44-03',
+            localPhone: '4-62, 3-46, 4-65',
+            email: 'ocpo@kemsu.ru'
+        },
+    },
+    funcLayoutProps: {
+        topbarAdditionalInfo: () => (
+            <div style={{ marginRight: '28px' }}>
+                <div style={{ textDecoration: 'none', fontSize: '13.6px'}}>TODO: выводить факультет</div>
+            </div>
+        )
     }
+
 })
 
 export { default as Layout } from '../../share/layout/Layout'
--- src/apps/home/index.js
+++ src/apps/home/index.js
@@ -2,15 +2,19 @@ import * as homePageModule from './HomePage'
 import { makeAppPageGenerator } from 'share/utils'
 
 export const getAppPage = makeAppPageGenerator('home', homePageModule, {
-    footerLinks: [
-        { title: 'Заявка на регистрацию', ext: true, url: '/assets/docs/Заявка на регистрацию в ЭИОС.doc', icon: 'registration' },
-        { title: 'Заявка на получение дополнительных прав', ext: true, url: '/assets/docs/Заявка на получение прав.doc', icon: 'rights' },
-        { title: 'Документы пользователю', url: '/manuals', icon: 'doc' }
-    ],
-    footerContactInfo: {
-        phone: '(384-2) 58-32-89, 58-33-41, 58-44-03',
-        localPhone: '4-62, 3-46, 4-65',
-        email: 'ocpo@kemsu.ru'
+    layoutProps: {
+        footerLinks: [
+            { title: 'Заявка на регистрацию', ext: true, url: '/assets/docs/Заявка на регистрацию в ЭИОС.doc', icon: 'registration' },
+            { title: 'Заявка на получение дополнительных прав', ext: true, url: '/assets/docs/Заявка на получение прав.doc', icon: 'rights' },
+            { title: 'Документы пользователю', url: '/manuals', icon: 'doc' }
+        ],
+        footerContactInfo: {
+            phone: '(384-2) 58-32-89, 58-33-41, 58-44-03',
+            localPhone: '4-62, 3-46, 4-65',
+            email: 'ocpo@kemsu.ru'
+        },
+        hideTopbar: true,
+        hideSidebar: true
     }
 })
 
