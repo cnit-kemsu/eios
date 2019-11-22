075fc785bb2ab6505dabd49cad7b29568b5ad502
Сотников Игорь Юрьевич <mxtfonlife@mail.ru>
2019-11-22 14:20:18 +0700

    Rename url

--- src/share/eios/studentPages.js
+++ src/share/eios/studentPages.js
@@ -1,5 +1,5 @@
 export const studentPages = selectedName => [
-    { url: '/personal-data', title: 'Личные данные', selected: selectedName === 'data' },
+    { url: '/student-personal-data', title: 'Личные данные', selected: selectedName === 'data' },
     { url: '/personal-area', title: 'Личный кабинет', selected: selectedName === 'personal' },
     { url: '/study-cards', title: 'Учебные карты', selected: selectedName === 'cards' },
 ]
