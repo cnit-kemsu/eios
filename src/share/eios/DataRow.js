6b3f5740f4558ab15f120cf06487238462454847
Сотников Игорь Юрьевич <mxtfonlife@mail.ru>
2019-12-06 11:19:24 +0700

    Update DataRow

--- src/share/eios/DataRow.js
+++ src/share/eios/DataRow.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 
-export function DataRow({ title, value }) {
-    return value ?
-        (<div style={{ padding: '6px 0px' }}>
-            <span style={{ width: '230px', display: 'inline-block', fontWeight: 'bold' }}>{title}: </span>
-            <span>{value}</span>
+export function DataRow({ title, value, content, children, titleStyle, contentStyle, style }) {
+
+    const c = value || content || children
+
+    return c ?
+        (<div style={{ padding: '6px 0px', ...style }}>
+            <span style={{ width: '230px', display: 'inline-block', fontWeight: 'bold', ...titleStyle }}>{title}: </span>
+            <span style={contentStyle}>{c}</span>
         </div>) : null
+}
+
+DataRow.defaultProps = {
+    style: {},
+    titleStyle: {},
+    contentStyle: {}
 }
